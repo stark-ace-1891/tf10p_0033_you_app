1d4cfb8df97c679709879e850eff7a61571f0ae3
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-06-22 18:16:11 -0500

    Avance

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:tf10p_0033_you_app/models/video_model.dart';
 import 'package:tf10p_0033_you_app/services/api_service.dart';
 import 'package:tf10p_0033_you_app/ui/general/colors.dart';
 import 'package:tf10p_0033_you_app/ui/widgets/item_filter_widget.dart';
-import 'package:tf10p_0033_you_app/ui/widgets/item_vide_widget.dart';
+import 'package:tf10p_0033_you_app/ui/widgets/item_video_widget.dart';
 
-class HomPage extends StatelessWidget {
-  
+class HomPage extends StatefulWidget {
+  @override
+  State<HomPage> createState() => _HomPageState();
+}
+
+class _HomPageState extends State<HomPage> {
   final ApiService _apiSercice = ApiService();
+  List<VideoModel> videos = [];
+
+  @override
+  initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() {
+    _apiSercice.getVideos().then((value) {
+      videos = value;
+      print(videos);
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-
     _apiSercice.getVideos();
 
     return SingleChildScrollView(
@@ -67,9 +86,14 @@ class HomPage extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-            ItemVideoWidget(),
-            ItemVideoWidget(),
-            ItemVideoWidget(),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: ScrollPhysics(),
+              itemCount: videos.length,
+              itemBuilder: (BuildContext context, int index){
+                return ItemVideoWidget(videoModel: videos[index],);
+              },
+            ),
           ],
         ),
       ),
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,13 +2,14 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:tf10p_0033_you_app/models/video_model.dart';
+import 'package:tf10p_0033_you_app/utils/constants.dart';
 
 
 class ApiService{
 
  Future<List<VideoModel>> getVideos() async{
     List<VideoModel> videosModel = [];
-    String _path = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDfJybqtNsrg-wMpc64w2Kd4_g_5SzwSpw&maxResults=50&regionCode=PE";
+    String _path = "$pathProduction/search?part=snippet&key=$apiKey&maxResults=50&regionCode=PE";
     Uri _uri = Uri.parse(_path);
 
     http.Response response = await http.get(_uri);
@@ -16,7 +17,6 @@ class ApiService{
       Map<String, dynamic> myMap = json.decode(response.body);
       List videos = myMap["items"];
       videosModel = videos.map((e) => VideoModel.fromJson(e)).toList();
-      print(videosModel);
     }
     return videosModel;
   }
